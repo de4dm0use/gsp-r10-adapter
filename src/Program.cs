94bae411b17d7431ef3ff37335e0abc16f0bc252
de4dm0use <whetton.oscar@gmail.com>
2026-09-17 20:14:26 +0100

    Launch the R10 tracker with the Windows UI

--- src/Program.cs
+++ src/Program.cs
@@ -1,33 +1,25 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.Windows.Forms;
 
 namespace gspro_r10
 {
-  class Program
+  internal static class Program
   {
+    [STAThread]
     public static void Main()
     {
-      
       IConfigurationBuilder builder = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory());
 
       if (File.Exists(Path.Join(Directory.GetCurrentDirectory(), "settings.json")))
-      {
         builder.AddJsonFile("settings.json");
-      }
       else
-      {
         BaseLogger.LogMessage($"settings.json file not found or could not be opened in {Directory.GetCurrentDirectory()}", "Main", LogMessageType.Error);
-      }
 
       IConfigurationRoot configuration = builder.Build();
-      
-      Console.Title = "GSP-R10 Connect";
-      BaseLogger.LogMessage("GSP - R10 Bridge starting. Press enter key to close", "Main");
+      ApplicationConfiguration.Initialize();
       ConnectionManager manager = new ConnectionManager(configuration);
-      Console.ReadLine();
-      BaseLogger.LogMessage("Shutting down...", "Main");
-      manager.Dispose();
-      BaseLogger.LogMessage("Exiting...", "Main");
+      Application.Run(new MainForm(manager));
     }
   }
 }
